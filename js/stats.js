f99dfb17b9f60ec9e5ae8da715abbb8cdb744474
Akzhar <akzharik@mail.ru>
2019-06-09 16:57:41 +0300

    reduce the number of function arg

--- js/stats.js
+++ js/stats.js
@@ -7,40 +7,44 @@ var CLOUD_HEIGHT = 270;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var GIST_MAX_HEIGHT = 150;
-var MY_BAR_COLOR = 'rgba(255, 0, 0, 1)';
+var GIST_LOW_LEVEL = CLOUD_HEIGHT - 20;
+var COLORS = {
+  WHITE: '#ffffff',
+  BLACK: '#000000',
+  GRAY: 'rgba(0, 0, 0, 0.7)',
+  RED: 'rgba(255, 0, 0, 1)'
+};
+var FONT = '16px PT Mono';
+var HEADER_TEXT = ['Ура вы победили!', 'Список результатов:'];
+var YOUR_NAME = 'Вы';
 
-function drawCloud(ctx, x, y, bgColor) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+function drawCloud(ctx, x, y) {
+  ctx.fillStyle = COLORS.GRAY;
   ctx.fillRect(x + 10, y + 10, CLOUD_WIDTH, CLOUD_HEIGHT);
-
-  ctx.fillStyle = bgColor;
+  ctx.fillStyle = COLORS.WHITE;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
-function drawCloudHeader(ctx, x, y, txtColor, font) {
-  ctx.fillStyle = txtColor;
-  ctx.font = font;
-  ctx.fillText('Ура вы победили!', x, y * 2);
-  ctx.fillText('Список результатов:', x, y * 3);
+function drawCloudHeader(ctx, x, y) {
+  ctx.fillStyle = COLORS.BLACK;
+  ctx.font = FONT;
+  ctx.fillText(HEADER_TEXT[0], x, y * 2);
+  ctx.fillText(HEADER_TEXT[1], x, y * 3);
 }
 
-function drawBar(ctx, x, gistLowLevel, name, value, i, valueMax, txtColor) {
-
-  var barX = x + (BAR_WIDTH + BAR_GAP) * i;
-  var barHeigth = value / valueMax * GIST_MAX_HEIGHT;
-  var barY = gistLowLevel - barHeigth;
+function drawBar(ctx, player, scoreMax) {
+  var barX = CLOUD_X + 30 + (BAR_WIDTH + BAR_GAP) * player.index;
+  var barHeigth = player.score / scoreMax * GIST_MAX_HEIGHT;
+  var barY = GIST_LOW_LEVEL - barHeigth;
   var barColor = 'rgba(0, 0, 255, ' + getRandom(0.1, 1) + ')';
-
   ctx.fillStyle = barColor;
-  if (name === 'Вы') {
-    ctx.fillStyle = MY_BAR_COLOR;
+  if (player.name === YOUR_NAME) {
+    ctx.fillStyle = COLORS.RED;
   }
-
-  ctx.fillRect(barX, barY, BAR_WIDTH, barHeigth); // отрисовка столбца графика
-  ctx.fillStyle = txtColor;
-  ctx.fillText(value, barX, barY - 10); // вывод очков игрока
-  ctx.fillText(name, barX, gistLowLevel + 20); // вывод имени игрока
-
+  ctx.fillRect(barX, barY, BAR_WIDTH, barHeigth);
+  ctx.fillStyle = COLORS.BLACK;
+  ctx.fillText(player.score, barX, barY - 10);
+  ctx.fillText(player.name, barX, GIST_LOW_LEVEL + 20);
 }
 
 function getRandom(min, max) {
@@ -48,16 +52,15 @@ function getRandom(min, max) {
 }
 
 window.renderStatistics = function (ctx, names, times) {
-
-  var valueMax = Math.round(Math.max.apply(null, times));
-  var gistLowLevel = CLOUD_HEIGHT - 20;
-  var contentX = CLOUD_X + 30;
-
-  drawCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-  drawCloudHeader(ctx, contentX, 20, '#000000', '16px PT Mono');
-
+  var scoreMax = Math.round(Math.max.apply(null, times));
+  drawCloud(ctx, CLOUD_X, CLOUD_Y);
+  drawCloudHeader(ctx, CLOUD_X + 30, 20);
   for (var i = 0; i < names.length; i++) {
-    drawBar(ctx, contentX, gistLowLevel, names[i], Math.round(times[i]), i, valueMax, '#000000');
+    var player = {
+      index: i,
+      name: names[i],
+      score: Math.round(times[i])
+    };
+    drawBar(ctx, player, scoreMax);
   }
-
 };
